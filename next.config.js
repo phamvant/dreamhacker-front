e54182cb70a4c32476a0d43d1795407f2dbd6551
phamvant <pham.t.286.01@gmail.com>
2023-11-30 18:43:16 +0700

    update cloud run

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    BCKEND_URL: "https://chrome-pointer-406515.as.r.appspot.com",
+    BCKEND_URL: "https://default-20231130t023634-kim7wgrrqq-de.a.run.app",
   },
   images: {
     remotePatterns: [
